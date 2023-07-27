aac55797d5f66609d21fb25697119e90a086a199
unknown <fashkenny>
2023-07-27 19:19:36 +0100

    extra

--- src/pages/LandingPage/MostDoneTask/MostDoneTask.tsx
+++ src/pages/LandingPage/MostDoneTask/MostDoneTask.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { styled } from 'styled-components'
-import ChartDone from './ChartDone'
-
+import ChartDone1 from './ChartDone1'
+import ChartDone2  from './ChartDone2'
+import  ChartDone3 from './ChartDone3'
 const MostTAsk = () => {
   return (
     <div>
         <Container>
-        <ChartDone/>
+        {/* <ChartDone1/> */}
+        <ChartDone2/>
+        {/* <ChartDone3/> */}
+      
         </Container>
         </div>
   )
@@ -16,7 +20,8 @@ export default MostTAsk
 const Container = styled.div`
 display:flex;
 width: calc(100% - 20%);
-height: 300px;
+height: 500px;
 padding-top: 50px;
 margin-left: 260px;
+
 `
